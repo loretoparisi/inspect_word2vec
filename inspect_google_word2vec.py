2c23d5bb2afc4e067fa9b00f85e794861dbb3785
Loreto Parisi <loretoparisi@gmail.com>
2016-10-21 13:59:33 +0200

    command line options

--- inspect_google_word2vec.py
+++ inspect_google_word2vec.py
@@ -3,18 +3,33 @@
 Created on Tue Apr 26 11:39:34 2016
 
 @author: Chris
+@lastauthor: Loreto Parisi (loretoparisi at gmail dot com)
 """
+
+import sys
 
 # Explore Google's huge Word2Vec model.
 
 import gensim
 import logging
 
+if len(sys.argv) <= 2:
+        text="\nUsage: python %s MODEL VOCABULARY" % (sys.argv[0])
+	text = text + "\nMODEL\t\tThe language model as binary format i.e. GoogleNews-vectors-negative300.bin"
+	text = text + "\nVOCABULARY\t\tThe vocabulary folder path i.e. ./vocabulary"
+
+	print text
+        exit();
+
+
 # Logging code taken from http://rare-technologies.com/word2vec-tutorial/
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # Load Google's pre-trained Word2Vec model.
-#model = gensim.models.Word2Vec.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)  
+datasetpath = sys.argv[1]
+vocabularypath = sys.argv[2]
+
+model = gensim.models.Word2Vec.load_word2vec_format(datasetpath, binary=True)  
 
 
 
@@ -34,7 +49,7 @@ wordsPerFile = int(100E3)
 # Write out the words in 100k chunks.
 for wordIndex in range(0, wordsInVocab, wordsPerFile):
     # Write out the chunk to a numbered text file.    
-    with open("vocabulary/vocabulary_%.2d.txt" % fileNum, 'w') as f:
+    with open(vocabularypath+"/vocabulary_%.2d.txt" % fileNum, 'w') as f:
         # For each word in the current chunk...        
         for i in range(wordIndex, wordIndex + wordsPerFile):
             # Write it out and escape any unicode characters.            
